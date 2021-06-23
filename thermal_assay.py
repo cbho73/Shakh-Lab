097a58e943d09d5d2a4739ea925427e111e20ced
Carl Ho <carlho@college.harvard.edu>
2021-06-23 12:15:01 -0400

    thermal_assay, added comments, melting temperature

--- thermal_assay.py
+++ thermal_assay.py
@@ -1,16 +1,9 @@
+from os import lseek
 import pandas as pd
 import matplotlib.pyplot as plt
+import csv
 
-rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-cols = range(1, 13)
-headers = []
-
-for num in cols:
-    headers.append([])
-    for letter in rows:
-        headers[-1].append(letter + str(num))
-print(len(headers))
-
+# reading the data into panda DataFrames
 path = "/Users/carlho/Documents/Shakh Lab/2021_06_21_L82V_hingemuts/"
 data = lambda fname : pd.read_excel(path + fname, 
                       sheet_name="FRET", 
@@ -22,16 +15,50 @@ df_amp = data("badkar_2021-06-21 16-31-27_CT010444 - " +
 df_deriv = data("badkar_2021-06-21 16-31-27_CT010444 - " + 
                 " Melt Curve Derivative Results.xlsx")
 
+# wanted header names in DataFrames
+rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+cols = range(1, 13)
+headers = []
+
+for num in cols:
+    headers.append([])
+    for letter in rows:
+        headers[-1].append(letter + str(num))
 col = [1, 2, 4, 5]  # columns where the samples are stored
-for n in col:
-    df1 = df_amp.loc[20:95, headers[n - 1][:7]]
-    df2 = df_deriv.loc[20:95, headers[n - 1][:7]]
-    molarity = "2" if n < 3 else "4"
-    trial = n if n < 3 else n - 3
-    
-    df1.plot();
-    plt.savefig(f"{path}Plots/{molarity}uM_t{str(trial)}")
-    df2.plot();
-    plt.savefig(f"{path}Plots/{molarity}uM_t{str(trial)}_derivs")
-    plt.xlabel('Temperature')
-plt.show();
+molarity = lambda n : "2" if n < 3 else "4"
+trial = lambda n : n if n < 3 else n - 3
+
+# print(len(headers))
+
+# plot and save graphs
+def plot():
+    # print(df_amp.head())
+    for n in col:
+        df1 = df_amp.loc[20:95, headers[n - 1][:7]]
+        df2 = df_deriv.loc[20:95, headers[n - 1][:7]]
+        
+        df1.plot();
+        # comment/uncomment line below to save files
+        plt.savefig(f"{path}Plots/{molarity(n)}uM_t{str(trial(n))}")
+        df2.plot();
+        # comment/uncomment line below to save files
+        plt.savefig(f"{path}Plots/{molarity(n)}uM_t{str(trial(n))}_derivs")
+        plt.xlabel('Temperature')
+    plt.show();
+
+def melting():
+    with open(path + 'tM_values.csv', 'w') as file:
+        writer = csv.writer(file)
+        for n in col:
+            names = []
+            row = []
+            for name in headers[n - 1][:7]:
+                s = df_deriv.loc[20:95, name]
+                names.append(f"{name}_{molarity(n)}uM_t{trial(n)}")
+                row.append(s.idxmin())
+                # print(f"{name}_{molarity(n)}uM_t{trial(n)}: {s.idxmin()}")
+            writer.writerow(names)
+            writer.writerow(row)
+
+# plot();  # uncomment to plot graphs
+melting();
